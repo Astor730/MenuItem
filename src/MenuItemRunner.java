ad523951ba53e645e69db20dc05fb0fc61ed5faf
BT_1N3_26 <mister.p730@gmail.com>
2018-11-21 14:20:28 -0500

    Done

--- src/MenuItemRunner.java
+++ src/MenuItemRunner.java
@@ -1,4 +1,18 @@
-public class MenuItemRunner
-{
+public class MenuItemRunner {
+    public static void main(String[] args) {
+        Sandwhich reuben = new Sandwhich("Reuben", 5.75);
+        Sandwhich bec = new Sandwhich("BEC", 2.00);
+        Salad waldorf = new Salad("Waldorf", 4.50);
+        Salad caeser = new Salad("Ceaser", 1.50);
+        Drink icedTea = new Drink("Iced Tea", 1.50);
+        Drink smoothie = new Drink("Smoothie", 5.50);
 
+        Trio trio1 = new Trio(reuben, waldorf, icedTea);
+        Trio trio2 = new Trio(bec, caeser, smoothie);
+        Trio trio3 = new Trio(bec, caeser, icedTea);
+
+        System.out.println(trio1.getName() + " Price: " + trio1.getPrice());
+        System.out.println(trio2.getName() + " Price: " + trio2.getPrice());
+        System.out.println(trio3.getName() + " Price: " + trio3.getPrice());
+    }
 }
